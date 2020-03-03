2529d25301a700fe58f94c3171cc8631c8bc3905
Gareth Emslie <gaemslie@microsoft.com>
2020-03-03 13:30:42 +0100

    refactor

--- Tasks/DownloadArtifactsNexus/DownloadArtifactsNexusV2/index.ts
+++ Tasks/DownloadArtifactsNexus/DownloadArtifactsNexusV2/index.ts
@@ -1,10 +1,9 @@
 import tl = require('azure-pipelines-task-lib/task');
-import url = require('url');
 import shell = require("shelljs");
 import path = require("path");
 import fs = require('fs');
-import http = require('http');
-import https = require('https');
+import { HttpHelper } from './HttpHelper';
+const helper = new HttpHelper();
 
 async function run() {
     console.log(`Downloading artifact.`);
@@ -20,7 +19,13 @@ async function run() {
         }
         
         // Get the service connection details for communicating with Nexus
-        const hostUri : string | undefined = tl.getEndpointUrl(connection, false);  
+        const hostUri : URL | undefined = new URL(tl.getEndpointUrl(connection, false));  
+
+        if(!hostUri)
+        {
+            throw new Error("A valid Nexus service connection Url is required!"); 
+        }
+
         const auth: tl.EndpointAuthorization | undefined = tl.getEndpointAuthorization(connection, false);
 
         if(!auth) {
@@ -43,7 +48,7 @@ async function run() {
         const baseVersion: string | undefined = tl.getInput("baseVersion", true);
         const packaging: string | undefined = tl.getInput("packaging", true);
         const classifier: string | undefined = tl.getInput("classifier", false);
-        let extension: string | undefined = tl.getInput("extension", false);
+        const extension: string | undefined = tl.getInput("extension", false);
         const downloadPath: string | undefined = tl.getInput("downloadPath", false);
 
         // Verify artifact download path is set
@@ -79,7 +84,7 @@ async function run() {
             tl.debug(`Agent proxy is set to '${agentProxy.proxyUrl}'.`);
         }
 
-        const requestUrl : url.UrlWithStringQuery = url.parse(hostUri);
+        tl.debug(`HostUri set to '${hostUri}'`);
         // https://repository.sonatype.org/nexus-restlet1x-plugin/default/docs/path__artifact_maven_redirect.html
         let requestPath : string = `/service/local/artifact/maven/redirect?r=${repository}&g=${group}&a=${artifact}&v=${baseVersion}&p=${packaging}`;
 
@@ -90,7 +95,7 @@ async function run() {
         }
         else
         {
-            tl.debug('Extension has not been supplied, set default packaging extension.');
+            tl.debug('Extension has not been supplied.');
         }
 
         // Do we have a classifier
@@ -102,121 +107,26 @@ async function run() {
         {
             tl.debug('Classifier has not been supplied.');
         }
+        
+        // Build the final search uri
+        const searchUri : URL = new URL(path.join(hostUri.pathname, requestPath), hostUri);
 
-        tl.debug(`Search for asset using '${url.resolve(requestUrl.href, requestPath)}'.`);
+        tl.debug(`Search for asset using '${searchUri}'.`);
         // need to refactor this logic to reduce duplication of code
-        if (hostUri.indexOf("https://") === 0) {
-            execute_https(requestUrl, requestPath, username, password, acceptUntrustedCerts);
+        if (searchUri.protocol === "https:")  {
+            helper.execute_https(searchUri, username, password, acceptUntrustedCerts);
         }
         else
         {
-            execute_http(requestUrl, requestPath, username, password);
+            helper.execute_http(searchUri, username, password);
         }
-        tl.debug(`Completing search for asset using '${url.resolve(requestUrl.href, requestPath)}'.`);
+        tl.debug(`Completing search for asset using '${searchUri}'.`);
 
     }
     catch (err) {
         tl.setResult(tl.TaskResult.Failed, err.message);
     }
     console.log(`Downloading artifact completed.`);
-}
-
-function execute_http(requestUrl : url.UrlWithStringQuery, requestPath : string, username : string, password : string)
-{
-    tl.debug(`execute_http.`);
-
-    const authBase64 : string = Buffer.from(username + ':' + password).toString('base64');
-
-    // Make sure the secret is correctly scrubbed from any logs
-    tl.setSecret(authBase64);
-
-    let options : http.RequestOptions = {
-        host: requestUrl.hostname,
-        path: requestPath,
-        method: 'GET',
-        headers: {
-            'Authorization': 'Basic ' + authBase64
-         }   
-    };
-
-    // Setup new agent dont use the global one
-    options.agent = new http.Agent(options);
-    options.port = requestUrl.port || options.defaultPort;
-   
-    // execute the http request
-    execute_request(http, options);
-}
-
-function execute_https(requestUrl : url.UrlWithStringQuery, requestPath : string, username : string, password : string, acceptUntrustedCerts : boolean)
-{
-    tl.debug(`execute_https.`);
-
-    const authBase64 : string = Buffer.from(username + ':' + password).toString('base64');
-
-    // Make sure the secret is correctly scrubbed from any logs
-    tl.setSecret(authBase64);
-
-    let options : https.RequestOptions = {
-        host: requestUrl.hostname,
-        path: requestPath,
-        method: 'GET',
-        rejectUnauthorized: !acceptUntrustedCerts, // By default ensure we validate SSL certificates
-        headers: {
-            'Authorization': 'Basic ' + authBase64
-         }   
-    };
-
-    // Setup new agent dont use the global one
-    options.agent = new https.Agent(options);
-    options.port = requestUrl.port || options.defaultPort;
-
-    // execute the https request
-    execute_request(https, options);
-}
-
-function execute_request(client : any, options :  http.RequestOptions | https.RequestOptions)
-{
-    tl.debug(`HTTP Request Options: ${JSON.stringify(options)}.`);  
-
-    let req : http.ClientRequest = client.request(options, function(res : http.IncomingMessage) {  
-        tl.debug(`HTTP Response Status Code: ${res.statusCode}.`);
-        tl.debug(`HTTP Response Headers: ${JSON.stringify(res.headers)}.`);
-
-        if (res.statusCode == 302) {
-            const downloadUrl : url.UrlWithStringQuery = url.parse(res.headers.location);
-
-            // Set correct options for the new request to download our file
-            options.host = downloadUrl.hostname;
-            options.path = downloadUrl.path;
-            options.port = downloadUrl.port || options.defaultPort;
-
-            tl.debug(`Download asset using '${downloadUrl.href}'.`);
-            let filename : string = path.basename(downloadUrl.pathname);
-            console.log(`Download filename '${filename}'`);
-
-            let inner_req : http.ClientRequest = client.request(options, function(inner_res : http.IncomingMessage) { 
-                tl.debug(`HTTP Response Status Code: ${inner_res.statusCode}.`);
-                tl.debug(`HTTP Response Headers: ${JSON.stringify(inner_res.headers)}.`);
-
-                if(inner_res.statusCode == 200)
-                {
-                    const file : fs.WriteStream = fs.createWriteStream(filename);
-                    inner_res.on('data', function(chunk : any){
-                        file.write(chunk);
-                    }).on('end', function(){
-                        file.end();
-                    });
-                    console.log(`Successfully downloaded asset '${filename}' using '${downloadUrl.href}'.`);
-                }
-            });
-            inner_req.end();
-        }else if (res.statusCode == 400) {
-            throw new Error(`Search returned multiple assets, please refine search criteria to find a single asset!`);
-        } else if (res.statusCode == 404) {
-            throw new Error(`Asset does not exist for search!`);
-        } 
-    });
-    req.end();
 }
 
 run();
---
+++ Tasks/DownloadArtifactsNexus/DownloadArtifactsNexusV3/HttpHelper.ts
@@ -0,0 +1,105 @@
+import tl = require('azure-pipelines-task-lib/task');
+import path = require("path");
+import fs = require('fs');
+import http = require('http');
+import https = require('https');
+
+export class HttpHelper {
+    public execute_http(searchUri : URL, username : string, password : string) : void
+    {
+        tl.debug(`execute_http.`);
+
+        const authBase64 : string = Buffer.from(username + ':' + password).toString('base64');
+
+        // Make sure the secret is correctly scrubbed from any logs
+        tl.setSecret(authBase64);
+
+        let options : http.RequestOptions = {
+            host: searchUri.hostname,
+            path: searchUri.pathname,
+            method: 'GET',
+            headers: {
+                'Authorization': 'Basic ' + authBase64
+            }   
+        };
+
+        // Setup new agent dont use the global one
+        options.agent = new http.Agent(options);
+        options.port = searchUri.port || options.defaultPort;
+    
+        // execute the http request
+        this.execute_request(http, options);
+    }
+
+    public execute_https(searchUri : URL, username : string, password : string, acceptUntrustedCerts : boolean) : void
+    {
+        tl.debug(`execute_https.`);
+
+        const authBase64 : string = Buffer.from(username + ':' + password).toString('base64');
+
+        // Make sure the secret is correctly scrubbed from any logs
+        tl.setSecret(authBase64);
+
+        let options : https.RequestOptions = {
+            host: searchUri.hostname,
+            path: searchUri.pathname,
+            method: 'GET',
+            rejectUnauthorized: !acceptUntrustedCerts, // By default ensure we validate SSL certificates
+            headers: {
+                'Authorization': 'Basic ' + authBase64
+            }   
+        };
+
+        // Setup new agent dont use the global one
+        options.agent = new https.Agent(options);
+        options.port = searchUri.port || options.defaultPort;
+
+        // execute the https request
+        this.execute_request(https, options);
+    }
+
+    private execute_request(client : any, options :  http.RequestOptions | https.RequestOptions) : void
+    {
+        tl.debug(`HTTP Request Options: ${JSON.stringify(options)}.`);  
+
+        let req : http.ClientRequest = client.request(options, function(res : http.IncomingMessage) {  
+            tl.debug(`HTTP Response Status Code: ${res.statusCode}.`);
+            tl.debug(`HTTP Response Headers: ${JSON.stringify(res.headers)}.`);
+
+            if (res.statusCode == 302) {
+                const downloadUri : URL = new URL(res.headers.location);
+
+                // Set correct options for the new request to download our file
+                options.host = downloadUri.hostname;
+                options.path = downloadUri.pathname;
+                options.port = downloadUri.port || options.defaultPort;
+
+                tl.debug(`Download asset using '${downloadUri}'.`);
+                let filename : string = path.basename(downloadUri.pathname);
+                console.log(`Download filename '${filename}'`);
+
+                let inner_req : http.ClientRequest = client.request(options, function(inner_res : http.IncomingMessage) { 
+                    tl.debug(`HTTP Response Status Code: ${inner_res.statusCode}.`);
+                    tl.debug(`HTTP Response Headers: ${JSON.stringify(inner_res.headers)}.`);
+
+                    if(inner_res.statusCode == 200)
+                    {
+                        const file : fs.WriteStream = fs.createWriteStream(filename);
+                        inner_res.on('data', function(chunk : any){
+                            file.write(chunk);
+                        }).on('end', function(){
+                            file.end();
+                        });
+                        console.log(`Successfully downloaded asset '${filename}' using '${downloadUri}'.`);
+                    }
+                });
+                inner_req.end();
+            }else if (res.statusCode == 400) {
+                throw new Error(`Search returned multiple assets, please refine search criteria to find a single asset!`);
+            } else if (res.statusCode == 404) {
+                throw new Error(`Asset does not exist for search!`);
+            } 
+        });
+        req.end();
+    }
+}
